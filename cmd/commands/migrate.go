ab66747700eb3755826d115bd1e36e97972709de
Tomas Zigo <tomas.zigo@slovanet.sk>
2023-06-24 09:07:19 +0200

    cmd/commands: add PostgresSQL DB statement_cache_mode config opt
    Fix error when PG DB connection pool is used:
    
    ```
    ERROR: prepared statement "lrupsc_1_6" already exists (SQLSTATE 42P05)
    ```
    
    https://pkg.go.dev/github.com/jackc/pgx/v4#ParseConfig

--- cmd/commands/migrate.go
+++ cmd/commands/migrate.go
@@ -17,12 +17,13 @@ import (
 func runMigrateCommand() error {
 	cfg := struct {
 		Postgres struct {
-			User     string `conf:"default:postgres"`
-			Password string `conf:"default:postgres,mask"`
-			Host     string `conf:"default:postgres"`
-			Name     string `conf:"default:postgres,env:POSTGRES_DB"`
-			Port     int    `conf:"default:5432"`
-			SSLMode  string `conf:"default:prefer"`
+			User               string `conf:"default:postgres"`
+			Password           string `conf:"default:postgres,mask"`
+			Host               string `conf:"default:postgres"`
+			Name               string `conf:"default:postgres,env:POSTGRES_DB"`
+			Port               int    `conf:"default:5432"`
+			SSLMode            string `conf:"default:prefer"`
+			StatementCacheMode string `conf:"default:prepare"`
 		}
 		Args conf.Args
 	}{}
@@ -39,6 +40,7 @@ func runMigrateCommand() error {
 	q := make(url.Values)
 	q.Set("sslmode", cfg.Postgres.SSLMode)
 	q.Set("timezone", "utc")
+	q.Set("statement_cache_mode", cfg.Postgres.StatementCacheMode)
 	u := url.URL{
 		Scheme:   "postgres",
 		User:     url.UserPassword(cfg.Postgres.User, cfg.Postgres.Password),
